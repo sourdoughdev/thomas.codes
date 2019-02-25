2459f05114c334bb7410fcd965c05622e447faf8
Thomas McGoey-Smith <thomas@tamcgoey.com>
2019-02-25 00:48:20 -0500

    fix templates

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -2,64 +2,38 @@ const ArticlePreview = ({ entry, widgetFor }) => {
   const data = entry.get('data').toJS()
   const date = dayjs(data.date).format('DD-MM-YY')
   return (
-    <div className="news-detail">
-      <div className="container">
-        <div className="row">
-          <div className="title">
-            <span className="date">{date}</span>
-            <h1>{data.title}</h1>
-            <p>{data.categories}</p>
-          </div>
-        </div>
-        <div className="row content">{widgetFor('body')}</div>
-      </div>
-    </div>
-  )
-}
+    <div className="sans-serif">
+      <h1>{data.title}</h1>
+      <div className="row content">{widgetFor('body')}</div>
+      <section id="subscribe newsletter">
+        <article class="cf mw8 center br2 bg-bar">
+          <div class="dt-ns dt--fixed-ns w-100">
+            <div class="pa3 pa4-ns dtc-ns v-mid">
+              <div>
+                <h2 class="fw4 white mt0 mb3">{data.newsletter.headline}</h2>
+                <p class="white-90 measure lh-copy mv0">
+                  {data.newsletter.body}
+                </p>
+              </div>
+            </div>
+            <div class="cf pa3 pa4-ns dtc-ns v-mid">
+              <form action="#">
+                <label class="mb1 white-90">Email Address</label>
 
-const ProductPreview = ({ entry, widgetFor }) => {
-  const data = entry.get('data').toJS()
-  return (
-    <div className="product-detail">
-      <div className="image" style={{ backgroundImage: `url('${data.image}')` }}></div>
-      <div className="content">
-        <div className="meta">
-          <h1>{data.title}</h1>
-          <div className="weight">
-            <span>{`${data.weight}kg`}</span>
-          </div>
-          <div className="price">
-            <span>{`€${data.price}`}</span>
-          </div>
-        </div>
-        <div className="body">{widgetFor('body')}</div>
-      </div>
-    </div>
-  )
-}
+                <div class="fl w-100 mt1">
+                  <input type="email" class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 br2" placeholder="newsletter@thomas.codes" type="text" name="email" value="" id="email-address" required />
+                </div>
 
-const HomePreview = ({ entry, widgetFor }) => {
-  const data = entry.get('data').toJS()
-  return (
-    <div>
-      <section className="hero" style={{ backgroundImage: `url('${data.hero.image}')` }}>
-        <div className="caption">
-          <div className="row">
-            <h1>{data.hero.title}</h1>
+                <div class="fl w-100 mt3">
+                  <input class="no-underline f5 tc db w-100 pv3 bg-animate bg-blue hover-bg-dark-blue white br2 pointer bn" type="submit" value="Subscribe" />
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="homepage-story">
-        <div className="container">
-          <div className="row">
-            <div className="story-content">{widgetFor('story')}</div>
-          </div>
-        </div>
+        </article>
       </section>
     </div>
   )
 }
 
-CMS.registerPreviewTemplate('article', ArticlePreview)
-CMS.registerPreviewTemplate('product', ProductPreview)
-CMS.registerPreviewTemplate('home', HomePreview)
+CMS.registerPreviewTemplate('articles', ArticlePreview)
